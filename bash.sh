6ac4f577a2035161b9f54265fb38a99a4a285d8d
Ahmed-Maher-Sabeq <ahmedmahersabeq@gmail.com>
2025-07-20 12:57:47 +0300

    rebased test

--- bash.sh
+++ bash.sh
@@ -8,6 +8,10 @@ echo "added a 3rd line"
 
 echo "added a 4th line"
 
+<<<<<<< HEAD
 echo "hello from test"
 echo "hello from main"
+=======
+echo "hello from dev"
+>>>>>>> 404db27 (added a hello from dev line to bash.sh)
 
